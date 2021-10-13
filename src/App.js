13ec662dbb6a47cfd4f58b29cd488066d0bc2661
Stiven Alvarez <jsalvareza@uqvirtual.edu.co>
2021-10-13 18:37:48 -0500

    cambio y prueba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Esto es una prueba mi so .
         </p>
         <a
           className="App-link"
